f1ecb0c91e8d9f0630d58563cafa20bd79fc12f5
Natalie Nardone <nardonen@grinnell.edu>
2024-12-12 16:15:26 -0600

    Implement Merge Sort

--- src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
@@ -8,6 +8,7 @@ import java.util.Comparator;
  * @param <T>
  *   The types of values that are sorted.
  *
+ * @author Natalie Nardone
  * @author Samuel A. Rebelsky
  */
 
@@ -55,6 +56,73 @@ public class MergeSorter<T> implements Sorter<T> {
    */
   @Override
   public void sort(T[] values) {
-    // STUB
+    mergeSortHelper(values, 0, values.length - 1);
   } // sort(T[])
+
+  /**
+   * Sorts an array from index l to index u in place using merge sort.
+   *
+   * @param values
+   *   an array to sort
+   * @param l
+   *   the starting index for sorting
+   * @param u
+   *   the ending index for sorting (inclusive)
+   */
+  public void mergeSortHelper(T[] values, int l, int u) {
+    if (u - l > 0) {
+      int midpoint = ((u - l) / 2) + l; // fix
+      mergeSortHelper(values, l, midpoint);
+      mergeSortHelper(values, midpoint + 1, u);
+      merge(values, l, u + 1, midpoint + 1); // merge values from l to r
+    } // if
+  } // mergeSortHelper(T[], int, int)
+
+  /**
+   * Merges two parts of an array together such that they are sorted,
+   * where the first array is stored from index l to m, exclusive,
+   * and the second array is stored from index m to u, exclusive.
+   *
+   * @param values
+   *   an array to sort
+   * @param l
+   *   the index where the first array starts
+   * @param u
+   *   the index where the second array ends
+   * @param m
+   *   the index where the first array ends/second array starts
+   */
+  public void merge(T[] values, int l, int u, int m) {
+    T[] newArray = values.clone();
+
+    int endLeft = m; // exclusive
+    int endRight = u; // exclusive
+    int curLeft = l;
+    int curRight = m;
+    int index = l;
+
+    while ((curLeft < endLeft) && (curRight < endRight)) {
+      if (order.compare(newArray[curLeft], newArray[curRight]) < 0) {
+        values[index] = newArray[curLeft];
+        curLeft++;
+      } else {
+        values[index] = newArray[curRight];
+        curRight++;
+      } // if/else
+      index++;
+    } // while
+
+    if (curLeft < endLeft) {
+      for (int i = index; i < u; i++) {
+        values[i] = newArray[curLeft];
+        curLeft++;
+      } // for
+    } // if
+    else if (curRight < endRight) {
+      for (int i = index; i < u; i++) {
+        values[i] = newArray[curRight];
+        curRight++;
+      } // for
+    } // if/else
+  } // merge(T[], int, int, int)
 } // class MergeSorter
